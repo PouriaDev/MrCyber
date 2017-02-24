c9bb91e8304bd96dfa03d4ad4c9a064281475905
p00ria <amir1300sobatdar@gmail.com>
2017-02-24 15:56:18 +0330

    Update and rename Plugins/Logo.php to plugins/Logo.php

--- plugins/Logo.php
+++ plugins/Logo.php
@@ -1,15 +1,12 @@
 <?php
-include "../Access.php";
-include "../index.php";
-/* The Source Bot: @NoMoBot
-Channel News: @Norbert_Team
-Support: @MosiDevBot
-*/
+include "../bot.php";
+include "../config.php";
+
 if(preg_match('/^\/([Ll]ogo) (.*)/s',$text)){
 	$text = str_replace('/Logo ','',$text);
 	$text = str_replace('/logo ','',$text);
 	$photo = file_get_contents('http://api.norbert-team.ir/photo-fa/?key=slm&size=50&color=white&text='.urlencode($text));
-	file_put_contents('Admin/Logo.png',$photo);
-	SendPhoto($chat_id , new CURLFILE('Admin/Logo.png'),"@Norbert_Team");
+	file_put_contents('data/Logo.png',$photo);
+	SendPhoto($chat_id , new CURLFILE('data/Logo.png'),"@MrCyberBot");
   }
   ?>
