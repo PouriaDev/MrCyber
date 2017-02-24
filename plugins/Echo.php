df039b73bbeb40c60b1c82de0c5a38ffdbe41e14
p00ria <amir1300sobatdar@gmail.com>
2017-02-24 15:53:07 +0330

    .

--- plugins/Echo.php
+++ plugins/Echo.php
@@ -1,10 +1,7 @@
 <?php
-include "../Access.php";
-include "../index.php";
-/* The Source Bot: @NoMoBot
-Channel News: @Norbert_Team
-Support: @MosiDevBot
-*/
+include "../bot.php";
+include "../config.php";
+
 if(preg_match('/^\/([Ee]cho) (.*)/s',$text)){
 	$text = str_replace('/Echo ','',$text);
 	$text = str_replace('/echo ','',$text);
