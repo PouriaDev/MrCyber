f31abab6575827c3d7e123ab24dce9e1c7a14009
p00ria <amir1300sobatdar@gmail.com>
2017-02-25 15:59:43 +0330

    Update and rename config.php to bot1.php

--- bot1.php
+++ bot1.php
@@ -8,15 +8,19 @@ $first_name = $message->from->first_name;
 $last_name = $message->from->last_name;
 $username = $message->from->userame;
 $text = $message->text;
+$photo = $update->message->photo;
+$audio = $update->message->audio;
+$document = $update->message->document;
+$sticker = $update->message->sticker;
+$video = $update->message->video;
+$voice = $update->message->voice;
 $forward = $update->message->forward_from;
 $reply = $update->message->reply_to_message;
 $block = file_get_contents("data/Block-List.txt");
 $start = file_get_contents("data/Start.txt");
 $help = file_get_contents("data/Help.txt");
 $command = file_get_contents("data/Command.txt");
-##------------------------------##
-// Enable Plugins
-##------------------------------##
+
 include "bot.php";
 include "plugins/Admin.php";
 include "plugins/Qr.php";
@@ -24,7 +28,7 @@ include "plugins/Calculator.php";
 include "plugins/KeepCalm.php";
 include "plugins/Photo.php";
 include "plugins/Echo.php";
-include "plugins/Date-Time.php";
+include "plugins/DateTime.php";
 include "plugins/Logo.php";
 include "plugins/Love.php";
 include "plugins/WebShot.php";
@@ -33,12 +37,10 @@ include "plugins/Short.php";
 include "plugins/Reply.php";
 include "plugins/Location.php";
 include "plugins/Sticker.php";
-include "plugins/Start-Help.php";
-##------------------------------##
+include "plugins/Start.php";
 if (strpos($block , "$from_id") !== false) {
 	return false;
 	}
-	##------------------------------##
 $user = file_get_contents('data/Member.txt');
     $members = explode("\n",$user);
     if (!in_array($chat_id,$members)){
@@ -46,5 +48,4 @@ $user = file_get_contents('data/Member.txt');
       $add_user .= $chat_id."\n";
      file_put_contents('data/Member.txt',$add_user);
     }unlink('error_log');
-	##------------------------------##
-	?>
+?>
